305a70c0971f670cec8e38acfb96e1df2eed3131
František Bereň <birko@live.com>
2014-05-06 16:46:11 +0200

    Import Files

---
+++ Graph.js
@@ -0,0 +1,176 @@
+﻿var Graph;
+(function (_Graph) {
+    var Point = (function () {
+        function Point(x, y, z) {
+            if (typeof x === "undefined") { x = 0; }
+            if (typeof y === "undefined") { y = 0; }
+            if (typeof z === "undefined") { z = 0; }
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
+            this.edges = new Array();
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        Point.prototype.toString = function () {
+            return "[" + this.x + "," + this.y + "," + this.z + "]";
+        };
+
+        Point.prototype.addEdge = function (point, unidirected) {
+            if (typeof unidirected === "undefined") { unidirected = false; }
+            if (!this.hasEdge(point)) {
+                this.edges[point.toString()] = new Edge(this, point, unidirected);
+                if (!unidirected) {
+                    point.addEdge(this, unidirected);
+                }
+            }
+            return this.getEdge(point);
+        };
+
+        Point.prototype.getEdge = function (point) {
+            if (this.hasEdge(point)) {
+                this.edges[point.toString()];
+            }
+            return undefined;
+        };
+
+        Point.prototype.hasEdge = function (point) {
+            return (this.edges[point.toString()] != undefined);
+        };
+
+        Point.prototype.removeEdge = function (point, unidirected) {
+            if (typeof unidirected === "undefined") { unidirected = false; }
+            if (this.hasEdge(point)) {
+                var edge = this.getEdge(point);
+                var index = this.edges.indexOf(edge);
+                if (index !== -1) {
+                    this.edges.splice(index, 1);
+                    if (!unidirected && !edge.unidirected) {
+                        point.removeEdge(this);
+                    }
+                    return edge;
+                }
+            }
+            return undefined;
+        };
+        return Point;
+    })();
+    _Graph.Point = Point;
+
+    var Edge = (function () {
+        function Edge(startPoint, endPoint, unidirected) {
+            if (typeof unidirected === "undefined") { unidirected = false; }
+            this.startPoint = new Point();
+            this.endPoint = new Point();
+            this.unidirected = false;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.unidirected = unidirected;
+        }
+        Edge.prototype.toString = function () {
+            return "[" + this.startPoint.toString() + this.endPoint.toString() + "]";
+        };
+        return Edge;
+    })();
+    _Graph.Edge = Edge;
+
+    var Graph = (function () {
+        function Graph() {
+            this.points = new Array();
+            this.edges = new Array();
+        }
+        Graph.prototype.hasPoint = function (point) {
+            return (this.points[point.toString()] != undefined);
+        };
+
+        Graph.prototype.addPoint = function (point) {
+            if (!this.hasPoint(point)) {
+                this.points[point.toString()] = point;
+            }
+        };
+
+        Graph.prototype.removePoint = function (point) {
+            if (this.hasPoint(point)) {
+                var index = this.points.indexOf(point);
+                if (index !== -1) {
+                    this.points.slice(index, 1);
+                    var _this = this;
+                    point.edges.forEach(function (edge) {
+                        _this.removeEdge(point, edge.endPoint, edge.unidirected);
+                    });
+                }
+            }
+        };
+
+        Graph.prototype.hasEdge = function (startPoint, endPoint) {
+            if (this.hasPoint(startPoint)) {
+                return startPoint.hasEdge(endPoint);
+            }
+            return false;
+        };
+
+        Graph.prototype.inEdges = function (edge) {
+            return (this.edges[edge.toString()] !== undefined);
+        };
+
+        Graph.prototype.addEdge = function (startPoint, endPoint, unidirected) {
+            if (typeof unidirected === "undefined") { unidirected = false; }
+            this.addPoint(startPoint);
+            this.addPoint(endPoint);
+            var edge = startPoint.addEdge(endPoint, unidirected);
+            if (edge !== undefined && !this.inEdges(edge)) {
+                this.edges[edge.toString()] = edge;
+                if (!unidirected) {
+                    edge = endPoint.getEdge(startPoint);
+                    if (edge !== undefined && !this.inEdges(edge)) {
+                        this.edges[edge.toString()] = edge;
+                    }
+                }
+            }
+        };
+
+        Graph.prototype.removeEdge = function (startPoint, endPoint, unidirected) {
+            if (typeof unidirected === "undefined") { unidirected = false; }
+            var edge = startPoint.removeEdge(endPoint, true);
+            if (this.inEdges(edge)) {
+                var index = this.edges.indexOf(edge);
+                if (index !== -1) {
+                    this.edges.splice(index, 1);
+                }
+            }
+            if (!unidirected) {
+                edge = endPoint.removeEdge(startPoint, true);
+                if (this.inEdges(edge)) {
+                    var index = this.edges.indexOf(edge);
+                    if (index !== -1) {
+                        this.edges.splice(index, 1);
+                    }
+                }
+            }
+        };
+
+        //shoortest path from point
+        Graph.prototype.dijsktra = function (point) {
+            var result = { dist: new Array(), previous: new Array() };
+            var infinity = 18437736874454810627;
+            this.points.forEach(function (value, index) {
+                result.dist[value.toString()] = infinity;
+                result.previous[value.toString()] = undefined;
+            });
+            result[point.toString()] = 0;
+            return result;
+        };
+
+        //shortest path for all points
+        Graph.prototype.flojdWarsshall = function () {
+        };
+
+        //minimum spanning tree
+        Graph.prototype.kruskal = function () {
+        };
+        return Graph;
+    })();
+    _Graph.Graph = Graph;
+})(Graph || (Graph = {}));
+//# sourceMappingURL=Graph.js.map
---
+++ Graph.ts
@@ -0,0 +1,163 @@
+﻿module Graph {
+    export class Point {
+        public x: number = 0;
+        public y: number = 0;
+        public z: number = 0;
+        public edges: Array<Edge> = new Array();
+
+        constructor(x:number = 0, y:number = 0, z:number = 0) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        toString():string {
+            return "[" + this.x + "," + this.y + "," + this.z + "]";
+        }
+
+        addEdge(point: Point, unidirected: boolean = false): Edge {
+            if (!this.hasEdge(point)) {
+                this.edges[point.toString()] = new Edge(this, point, unidirected);
+                if (!unidirected) {
+                    point.addEdge(this, unidirected);
+                }
+            }
+            return this.getEdge(point);
+        }
+
+        getEdge(point: Point): Edge {
+            if (this.hasEdge(point)) {
+                this.edges[point.toString()]
+            }
+            return undefined;
+        }
+
+        hasEdge(point: Point): boolean {
+            return (this.edges[point.toString()] != undefined);
+        }
+
+        removeEdge(point: Point, unidirected: boolean = false):Edge {
+            if (this.hasEdge(point)) {
+                var edge = this.getEdge(point);
+                var index = this.edges.indexOf(edge);
+                if (index !== -1) {
+                    this.edges.splice(index, 1);
+                    if (!unidirected && !edge.unidirected) {
+                        point.removeEdge(this);
+                    }
+                    return edge;
+                }
+            }
+            return undefined;
+        }
+    }
+
+    export class Edge {
+        public startPoint: Point = new Point();
+        public endPoint: Point = new Point();
+        public unidirected: boolean = false;
+
+        constructor(startPoint, endPoint: Point, unidirected: boolean = false) {
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            this.unidirected = unidirected;
+        }
+
+        toString():string {
+            return "[" + this.startPoint.toString() + this.endPoint.toString() + "]";
+        }
+    }
+
+    export class Graph {
+        public points: Array<Point> = new Array();
+        public edges: Array<Edge> = new Array();
+
+        hasPoint(point: Point): boolean {
+            return (this.points[point.toString()] != undefined);
+        }
+
+        addPoint(point: Point) {
+            if (!this.hasPoint(point)) {
+                this.points[point.toString()] = point;
+            }
+        }
+
+        removePoint(point: Point) {
+            if (this.hasPoint(point)) {
+                var index = this.points.indexOf(point);
+                if (index !== -1) {
+                    this.points.slice(index, 1);
+                    var _this = this;
+                    point.edges.forEach(function (edge) {
+                        _this.removeEdge(point, edge.endPoint, edge.unidirected);
+                    });
+                }
+            }
+        }
+
+        hasEdge(startPoint, endPoint: Point): boolean {
+            if (this.hasPoint(startPoint)) {
+                return startPoint.hasEdge(endPoint);
+            }
+            return false;
+        }
+
+        inEdges(edge:Edge): boolean {
+            return (this.edges[edge.toString()] !== undefined);
+        }
+
+        addEdge(startPoint, endPoint: Point, unidirected: boolean = false) {
+            this.addPoint(startPoint);
+            this.addPoint(endPoint);
+            var edge = startPoint.addEdge(endPoint, unidirected);
+            if (edge !== undefined && !this.inEdges(edge)) {
+                this.edges[edge.toString()] = edge;
+                if (!unidirected) {
+                    edge = endPoint.getEdge(startPoint);
+                    if (edge !== undefined && !this.inEdges(edge)) {
+                        this.edges[edge.toString()] = edge;
+                    }
+                }
+            }
+        }
+
+        removeEdge(startPoint, endPoint: Point, unidirected: boolean = false) {
+            var edge = startPoint.removeEdge(endPoint, true);
+            if (this.inEdges(edge)) {
+                var index = this.edges.indexOf(edge);
+                if (index !== -1) {
+                    this.edges.splice(index, 1);
+                }
+            }
+            if (!unidirected) {
+                edge = endPoint.removeEdge(startPoint, true);
+                if (this.inEdges(edge)) {
+                    var index = this.edges.indexOf(edge);
+                    if (index !== -1) {
+                        this.edges.splice(index, 1);
+                    }
+                }
+            }
+        }
+
+        //shoortest path from point
+        dijsktra(point: Point): { dist: Array<number>; previous: Array<Point> } {
+            var result = { dist: new Array(), previous: new Array() };
+            var infinity = 18437736874454810627;
+            this.points.forEach(function (value: Point, index: number) {
+                result.dist[value.toString()] = infinity;
+                result.previous[value.toString()] = undefined;
+            });
+            result[point.toString()] = 0;
+            return result;
+        }
+
+        //shortest path for all points
+        flojdWarsshall() {
+        }
+
+        //minimum spanning tree
+        kruskal() {
+        }
+    }
+}
